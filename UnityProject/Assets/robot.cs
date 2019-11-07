579a4019ed045677dee341f55231c97956f73e70
26380097 <57471105+26380097@users.noreply.github.com>
2019-11-07 12:04:09 +0800

    finish

---
+++ UnityProject/Assets/robot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    [Header("對話")]
+    public string Say = "早啊啊啊啊啊";
+    public float SaySpeed = 0.2f;
+
+    [Header("任務")]
+    public bool Complete = false;
+    public int Batery, BateryFinish = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
